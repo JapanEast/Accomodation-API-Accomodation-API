a423d025e2de37f5ae6d2bc9b6528ee953bafc10
JapanEast <106453845+JapanEast@users.noreply.github.com>
2020-11-22 19:47:07 +0000

    New files.

--- client/game.go
+++ client/game.go
@@ -5,4 +5,11 @@ import (
 	"fmt"
 
 	"github.com/google/uuid"
-	"github.com/rwsargent/boardbot
+	"github.com/rwsargent/boardbots-go/internal"
+)
+
+// game.go is responsible for the game specific APIs in boardbots.dev
+
+type (
+	Game[S any] struct {
+		Id        uu

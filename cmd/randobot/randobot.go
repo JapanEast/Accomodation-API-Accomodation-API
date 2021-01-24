9662364c16aeb932a2565d934241946a2cc65ee1
JapanEast <106453845+JapanEast@users.noreply.github.com>
2021-01-24 13:45:07 +0000

    Version up

--- cmd/randobot/randobot.go
+++ cmd/randobot/randobot.go
@@ -1,2 +1,6 @@
 // randobot.go makes random moves for the given lockitdown game.
-// You need to run randobot with a username, gameI
+// You need to run randobot with a username, gameId, and server address.
+//
+// $> randobot -username=randobot -gameId=00000000-...-0000 -server=https://boardbots.dev
+//
+// It is recommend 

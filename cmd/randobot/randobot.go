86c8b5f52700e2fcfff59e68eae06ac70c21a27e
JapanEast <106453845+JapanEast@users.noreply.github.com>
2021-02-21 21:50:47 +0000

    usb: option: add Alcatel L800MA

--- cmd/randobot/randobot.go
+++ cmd/randobot/randobot.go
@@ -20,4 +20,6 @@ import (
 var edges = []struct{ pos, dir internal.Position }{
 	{pos: internal.Position{Q: 0, R: -5}, dir: internal.Position{Q: 0, R: 1}},
 	{pos: internal.Position{Q: 5, R: -5}, dir: internal.Position{Q: -1, R: 1}},
-	{pos: internal.Position{Q: 5, R: 0}, dir: internal.Position{Q: -1, R: 0}}
+	{pos: internal.Position{Q: 5, R: 0}, dir: internal.Position{Q: -1, R: 0}},
+	{pos: internal.Position{Q: 0, R: 5}, dir: internal.Position{Q: 0, R: -1}},
+	{pos: internal.Position{Q: -5, R: 5}, dir: internal.Position{Q: 1

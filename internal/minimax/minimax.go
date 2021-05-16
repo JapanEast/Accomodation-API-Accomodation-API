d4c77ef9916fc11d86198f6138a3299356121e0d
JapanEast <106453845+JapanEast@users.noreply.github.com>
2021-05-16 19:00:12 +0000

    Rename README to README.rst

--- internal/minimax/minimax.go
+++ internal/minimax/minimax.go
@@ -11,4 +11,13 @@ type (
 		Children([]Node) []Node
 		Release()
 		Move()
-		Un
+		Undo()
+		ShouldMaximize() bool
+		Score() int
+		SetScore(int)
+	}
+)
+
+var nodePool = sync.Pool{
+	New: func() any {
+		buf := make([]Node, 0, 

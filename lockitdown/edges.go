55481d9c84b820e0d6316a1fcb42f37765c59bd7
JapanEast <106453845+JapanEast@users.noreply.github.com>
2021-05-23 12:14:18 +0000

    Merging from branches/1.3

--- lockitdown/edges.go
+++ lockitdown/edges.go
@@ -2,4 +2,13 @@ package lockitdown
 
 import "sort"
 
-var cache map[int][]Placement = make(map[int][]Placeme
+var cache map[int][]Placement = make(map[int][]Placement)
+
+type Placement struct {
+	position  Pair
+	direction Pair
+}
+
+type ByCorner []Placement
+
+func (a ByCorner) Len() int     

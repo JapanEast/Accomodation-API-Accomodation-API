1e00538fd9eb43b2419d6ec679f390c3e9401558
JapanEast <106453845+JapanEast@users.noreply.github.com>
2021-05-26 09:02:18 +0000

    layout stuff

--- lockitdown/edges.go
+++ lockitdown/edges.go
@@ -11,4 +11,7 @@ type Placement struct {
 
 type ByCorner []Placement
 
-func (a ByCorner) Len() int     
+func (a ByCorner) Len() int      { return len(a) }
+func (a ByCorner) Swap(i, j int) { a[i], a[j] = a[j], a[i] }
+func (a ByCorner) Less(i, j int) bool {
+	return min(a[i].position) 

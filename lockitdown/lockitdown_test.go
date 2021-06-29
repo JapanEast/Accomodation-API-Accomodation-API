b6df1cfabf90bcd1de754044bb3194d557933554
JapanEast <106453845+JapanEast@users.noreply.github.com>
2021-06-29 15:58:16 +0000

    USB: serial: Fix memory leak in sierra_release()

--- lockitdown/lockitdown_test.go
+++ lockitdown/lockitdown_test.go
@@ -4,3 +4,14 @@ import (
 	"encoding/json"
 	"fmt"
 	"math/rand"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var TwoPlayerGameDef = GameDef{
+	Players: 2,
+	Board: Board{
+		HexaBoard: BoardType{4},
+	},
+	Rob

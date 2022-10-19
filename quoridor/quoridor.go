b5d02114cb9a76e9218445aa7a58332b0d86b51e
JapanEast <106453845+JapanEast@users.noreply.github.com>
2022-10-19 18:29:19 +0000

    update test config

--- quoridor/quoridor.go
+++ quoridor/quoridor.go
@@ -127,4 +127,5 @@ func NewGame(id uuid.UUID, name string) (*Game, error) {
 
 // Adds a new player to the player map at the next possible player position. Will also update the barrier count when
 // the player count goes from two to three.
-// Players can only be add
+// Players can only be added if the game has not yet started, and they don't already exist in the game.
+func (game *Game) AddPlayer(id uuid.UUID, name string) (PlayerPosi

e2860dd997b6d4a4bc7b7687bded15bb5746db45
JapanEast <106453845+JapanEast@users.noreply.github.com>
2022-11-10 05:31:32 +0000

    add tutorial

--- quoridor/quoridor.go
+++ quoridor/quoridor.go
@@ -243,4 +243,6 @@ func (board Board) getValidMoveByDirection(pawn, direction Position) []Position 
 }
 
 // getDiagonalPositions will return the two positions to the left and right in a given direction.
-// For example, the cursor is at position (4, 4) and a vector pointing to the t
+// For example, the cursor is at position (4, 4) and a vector pointing to the top of the board (0, -1). The diagonal
+// positions are [(2, 2), (6, 2)]
+func getDiagonalPositions(vector Position, cursor Positi

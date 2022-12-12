849ac536244017634ae66ed87894c7ae9ae17ba1
JapanEast <106453845+JapanEast@users.noreply.github.com>
2022-12-12 05:41:04 +0000

    Add missing use statement

--- quoridor/quoridor_test.go
+++ quoridor/quoridor_test.go
@@ -9,4 +9,7 @@ import (
 
 var TestIds = []uuid.UUID{
 	uuid.MustParse("98ae983e-3f04-42ab-928a-c399d6d18375"),
-	uuid.MustParse("5341acab-6e28-4d28-8530-8716e0c3dd
+	uuid.MustParse("5341acab-6e28-4d28-8530-8716e0c3dd2e"),
+	uuid.MustParse("790bcc3f-6e72-4a0e-a6ea-bc806aa8aa03"),
+	uuid.MustParse("6c8420b5-e7f5-4328-ae29-4dbdf7537612"),
+	uuid.Must
